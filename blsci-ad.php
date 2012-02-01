ba7e8165cda884ed4407bbebc6390eb917e55b79
Boone Gorges <boonebgorges@gmail.com>
2012-02-01 16:30:03 -0500

    Make sure plugin-fix is loaded late

--- blsci-ad.php
+++ blsci-ad.php
@@ -17,7 +17,7 @@ define( 'ADBB_INCLUDES_PATH', ADBB_INSTALL_PATH . 'includes/' );
 
 // Include the relevant pieces
 include( ADBB_INCLUDES_PATH . 'schema.php' ); // Sets up the custom post type, etc
-include( ADBB_INCLUDES_PATH . 'plugin-fix.php' ); // Fixes for the AD Integration plugin
+add_action( 'plugins_loaded', create_function( '', "include( '" . ADBB_INCLUDES_PATH . "plugin-fix.php' );" ), 999 ); // Fixes for the AD Integration plugin
 include( ADBB_INCLUDES_PATH . 'class-adldap.php' ); // The AD API class
 include( ADBB_INCLUDES_PATH . 'username-backpat.php' ); // Backpat for pre-AD usernames
 
