e2d046c1af1af73cd4c94a2eb19a90b7a7ab173e
Boone Gorges <boonebgorges@gmail.com>
2012-02-02 11:35:19 -0500

    Plugin order

--- blsci-ad.php
+++ blsci-ad.php
@@ -15,11 +15,14 @@ Version: 1.0
 define( 'ADBB_INSTALL_PATH', dirname( __FILE__ ) . '/' );
 define( 'ADBB_INCLUDES_PATH', ADBB_INSTALL_PATH . 'includes/' );
 
-// Include the relevant pieces
-include( ADBB_INCLUDES_PATH . 'schema.php' ); // Sets up the custom post type, etc
-add_action( 'plugins_loaded', create_function( '', "include( '" . ADBB_INCLUDES_PATH . "plugin-fix.php' );" ), 999 ); // Fixes for the AD Integration plugin
-include( ADBB_INCLUDES_PATH . 'class-adldap.php' ); // The AD API class
-include( ADBB_INCLUDES_PATH . 'username-backpat.php' ); // Backpat for pre-AD usernames
+// Load plugin files late, as they depend on the AD plugin
+function blsci_ad_includes() {
+	include( ADBB_INCLUDES_PATH . 'schema.php' ); // Sets up the custom post type, etc
+	include( ADBB_INCLUDES_PATH . 'plugin-fix.php' ); // Fixes for the AD Integration plugin
+	include( ADBB_INCLUDES_PATH . 'class-adldap.php' ); // The AD API class
+	include( ADBB_INCLUDES_PATH . 'username-backpat.php' ); // Backpat for pre-AD usernames
+}
+add_action( 'plugins_loaded', 'blsci_ad_includes', 999 );
 
 if ( is_network_admin() || is_admin() ) {
 	include( ADBB_INCLUDES_PATH . 'admin.php' );
